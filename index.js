ff8fd946f92fd2ef59fc2c276dfac8bc1d7ed815
hisa <m.hisa3153@gmail.com>
2018-03-10 21:15:59 +0900

    fix index.js

--- index.js
+++ index.js
@@ -18,22 +18,6 @@ var STOP_MESSAGE = "さようなら";
 //=========================================================================================================================================
 //「TODO: ここから下のデータを自分用にカスタマイズしてください。」
 //=========================================================================================================================================
-<<<<<<< HEAD
-const data = [
-    'A year on Mercury is just 88 days long.',
-    'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-    'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-    'On Mars, the Sun appears about half the size as it does on Earth.',
-    'Earth is the only planet not named after a god.',
-    'Jupiter has the shortest day of all the planets.',
-    'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-    'The Sun contains 99.86% of the mass in the Solar System.',
-    'The Sun is an almost perfect sphere.',
-    'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-    'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-    'The temperature inside the Sun can reach 15 million degrees Celsius.',
-    'The Moon is moving approximately 3.8 cm away from our planet every year.'
-=======
 var data = [
     "水星の一年はたった88日です。",
     "金星は水星と比べて太陽より遠くにありますが、気温は水星よりも高いです。",
@@ -47,7 +31,6 @@ var data = [
     "土星は自身が太陽から受けるエネルギーの2.5倍のエネルギーを宇宙に放出しています。",
     "太陽の内部温度は摂氏1500万度にも達します。",
     "月は毎年3.8cm地球から離れていっています。"
->>>>>>> b32e5f5bdd0148db3b9da131fa5ab263366a6e8d
 ];
 
 //=========================================================================================================================================
